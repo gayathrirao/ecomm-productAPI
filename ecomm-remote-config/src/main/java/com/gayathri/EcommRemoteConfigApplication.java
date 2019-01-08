aded29067e44f5d548ac4ad079e4321642f3d1f5
Gayathri B S <gayrao@publicisgroupe.net>
2019-01-08 16:58:37 +0530

    btch 1 changes - mostly remote config

--- ecomm-remote-config/src/main/java/com/gayathri/EcommRemoteConfigApplication.java
+++ ecomm-remote-config/src/main/java/com/gayathri/EcommRemoteConfigApplication.java
@@ -2,14 +2,14 @@ package com.gayathri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class EcommProductApiApplication {
+@EnableConfigServer
+public class EcommRemoteConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommProductApiApplication.class, args);
+		SpringApplication.run(EcommRemoteConfigApplication.class, args);
 	}
 
 }
