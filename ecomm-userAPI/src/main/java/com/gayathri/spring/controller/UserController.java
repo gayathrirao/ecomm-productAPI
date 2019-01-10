583e86bff5d463620e4979e41a2cb5a521eee903
Gayathri B S <gayrao@publicisgroupe.net>
2019-01-10 15:38:54 +0530

    put and delete operations on user api

--- ecomm-userAPI/src/main/java/com/gayathri/spring/controller/UserController.java
+++ ecomm-userAPI/src/main/java/com/gayathri/spring/controller/UserController.java
@@ -6,9 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +41,20 @@ public class UserController {
 		User savedUser = userService.save(usr);
 		return (ResponseEntity.ok("User saved successfully" + savedUser ));
 	}
+	
+	   @PutMapping("/user/{user_name}")
+	   public ResponseEntity<?> update(@PathVariable("user_name") String userName, @RequestBody User user) {
+	      
+		  userService.update(userName, user);
+	      
+	      return ResponseEntity.ok().body("User has been updated successfully.");
+	   }
+
+	   /*---Delete a Product by id---*/
+	   @DeleteMapping("/user/{user_name}")
+	   public ResponseEntity<?> delete(@PathVariable("user_name") String username) {
+	      userService.delete(username);
+	      return ResponseEntity.ok().body("User has been deleted successfully.");
+	   }
+	   
 }
